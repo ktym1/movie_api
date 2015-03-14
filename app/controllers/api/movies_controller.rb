1fac807060c257dedc3092da6443085816995d41
Kerry Mui <kerry.t.mui@gmail.com>
2015-03-13 23:00:22 -0400

    deleted old movies_controller file; updated movies_controller file under api folder by deleting the api module and namespacing api instead. Works bc removed api subdomain constraint in routes

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,31 +1,29 @@
-module API
-  class MoviesController < ApplicationController
+class API::MoviesController < ApplicationController
 
-    def index
-      movies = Movie.all
-      
-      respond_to do |format|
-        format.html { render html: movies, status: 200 }
-        format.json { render json: movies, status: 200 }
-        format.xml { render xml: movies, status: 200 }
-      end
+  def index
+    movies = Movie.all
+    
+    respond_to do |format|
+      format.html { render html: movies, status: 200 }
+      format.json { render json: movies, status: 200 }
+      format.xml { render xml: movies, status: 200 }
     end
-
-    def show
-      movie = Movie.find(params[:id])
-      render json: movie, status: 200
-    end
-
-    def create
-      movie = Movie.new(movie_params)
-      if movie.save
-        render json: movie, status:201
-      end
-    end
-
-    private
-      def movie_params
-        params.require(:movie).permit(:title, :director, :year)
-      end
   end
+
+  def show
+    movie = Movie.find(params[:id])
+    render json: movie, status: 200
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+    if movie.save
+      render json: movie, status:201
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :director, :year)
+    end
 end
--- app/controllers/movies_controller.rb
+++
@@ -1,8 +0,0 @@
-class MoviesController < ApplicationController
-
-  def index
-    movies = Movie.all
-    render json: movies, status: 200
-  end
-
-end
